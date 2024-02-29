48e83da1fab60c722a033ae83c6fc777dbde699b
Jordan-Reeves <jar5927@RIT.edu>
2024-02-29 09:42:55 -0500

    Added Ardity, fix gitignore

--- Cocoon/Assets/scripts/MyListener.cs
+++ Cocoon/Assets/scripts/MyListener.cs
@@ -5,32 +5,75 @@ using UnityEngine;
 public class MyListener : MonoBehaviour
 {
     public GameObject CameraModifier;
-    float oldData;
-    float newData;
+    float dialPercentPrevious = 0;
+
+    public float dialPercent;
+    public float switchData;
+
+    bool cameraForward = false;
+    ArduinoDataClass arduinoData = new ArduinoDataClass();
 
     // Use this for initialization
     void Start()
     {
 
     }
+
     //Update is called once per frame
     void Update()
     {
     }
-    // Invoked when a line of data is received from the serial device.
+
+    //Invoked when a line of data is received from the serial device.
     void OnMessageArrived(string msg)
     {
-        Debug.Log("Arduino Data: " + msg);
-        newData = float.Parse(msg);
-        //oldData = newData;
-        float changeHeight = 50 - newData;
+        //Debug.Log("Arduino Data: " + msg);
+        //parseArduinoData("Dial:100,Switch:0");
+        parseArduinoData(msg);
 
-        CameraModifier.transform.position += new Vector3(0,0,Time.deltaTime * changeHeight);
+        switchData = arduinoData.Switch;
+
+        dialPercent = arduinoData.Dial;
+        float cameraPos = 0;
+
+
+        if (dialPercent != dialPercentPrevious && (Mathf.Abs(dialPercentPrevious - dialPercent) > 3))
+        {
+            //Debug.Log("upadated Old " + oldData);
+            cameraForward = dialPercentPrevious > dialPercent;
+            dialPercentPrevious = dialPercent;
+            cameraPos = Mathf.Abs(50 - dialPercent);
+            if (!cameraForward)
+            {
+                cameraPos = (cameraPos * -1.00f);
+            }
+        }
+
+        //Debug.Log("cameraForward " + cameraForward);
+        //Debug.Log("cameraPos " + cameraPos);
+        CameraModifier.transform.position += new Vector3(0, 0, cameraPos * 0.5f);
 
 
 
 
     }
+    //Data is a string in this format: "Dial:100,Switch:0"
+    void parseArduinoData(string data)
+    {
+        string[] ardData = data.Split(",");
+
+        arduinoData.Dial = float.Parse(ardData[0].Split(":")[1]);
+        arduinoData.Switch = float.Parse(ardData[1].Split(":")[1]);
+        //Debug.Log("Dial: " + arduinoData.Dial);
+        //Debug.Log("Switch: " + arduinoData.Switch);
+    }
+
+    public class ArduinoDataClass
+    {
+        public float Dial;
+        public float Switch;
+    }
+
     // Invoked when a connect/disconnect event occurs. The parameter 'success'
     // will be 'true' upon connection, and 'false' upon disconnection or
     // failure to connect.
