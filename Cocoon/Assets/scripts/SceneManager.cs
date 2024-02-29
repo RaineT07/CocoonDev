01165b8d61a42f34639f4d8434f0e5584eee70bd
RaineT07 <srt1849@rit.edu>
2024-02-28 20:07:14 -0500

    Implemented Scene manager & scene set
    The Scene set is a prefab which has 2 arrays, one for bg/mg/fg and one for assets. Right now, there are 2 in the active sample scene. One for oasis one for Tropic.
    Scene manager object tracks the arduino listener, as well as the 3 sets of objects that represent the background, midground, and foreground layer. It also has a public field for the starting sceneSet. This will eventually be swapped and updated using the arduino input.

---
+++ Cocoon/Assets/scripts/SceneManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // [SerializeField]
+    // public ArduinoListener arduinoListener;
+    public SceneSet sceneSet;
+    public GameObject[] layers;
+    public GameObject ArduinoListener;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<layers.Length; i++)
+        {
+            layers[i].GetComponent<Renderer>().material.mainTexture = sceneSet.textures[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Cocoon/Assets/scripts/SceneSet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSet : MonoBehaviour
+{
+    public Texture2D[] textures;
+    public Texture2D[] assetTextures;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
