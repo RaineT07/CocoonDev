4d8d66b9034a610a66c0c7195bb5bd0bf894ce1f
CursedMushroom <osl2877@rit.edu>
2024-03-04 20:18:34 -0500

    Added cam switch on button press
    key press 1 and 2 to flip between cameras

--- Cocoon/Assets/scripts/CameraDisplayScript.cs
+++ Cocoon/Assets/scripts/CameraDisplayScript.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExampleScript : MonoBehaviour
+public class CamDisplayScript : MonoBehaviour
 {
     public Camera tropicCamera;
     public Camera oasisCamera;
 
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))  // Pressed 1
+        {
+            ShowTropic();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))  // Pressed 2
+        {
+            ShowOasis();
+        }
+    }
 
     public void ShowOasis()
     {
