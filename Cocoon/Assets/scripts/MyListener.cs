875cf292daab51ff1ac2a4c2ab6f7f26fe7bce18
Jordan-Reeves <jar5927@RIT.edu>
2024-03-18 17:29:03 -0400

    Got camera zoom & hue change working

--- Cocoon/Assets/scripts/MyListener.cs
+++ Cocoon/Assets/scripts/MyListener.cs
@@ -6,15 +6,22 @@ using UnityEngine;
 public class MyListener : MonoBehaviour
 {
     public GameObject CameraModifier;
-    float dialPercentPrevious = 0;
 
+    public float quadrantData;
     public float dialPercent;
     public float switchData;
     public float bttnData;
     public float proxData;
 
+    float dialPercentPrevious = 0;
+    float proxPrevious = 0;
+
     public Camera tropicCamera;
     public Camera oasisCamera;
+    public Camera spaceCamera;
+
+    float zoomMin = 0;
+    float zoomMax = -100;
 
     public bool currCamera = true; // true = oasis, false = tropic
     float prevBttnData;
@@ -22,9 +29,12 @@ public class MyListener : MonoBehaviour
     bool buttonPressed = false;
 
     public GameObject[] animations;
+    public GameObject[] backgrounds;
+    public GameObject[] midgrounds;
+    public GameObject[] foregrounds;
 
 
-    bool cameraForward = false;
+    bool cameraForward = false; // true = prev > curr, false = curr > prev
     ArduinoDataClass arduinoData = new ArduinoDataClass();
 
     // Use this for initialization
@@ -32,12 +42,19 @@ public class MyListener : MonoBehaviour
     {
         startCams();
 
-
     }
 
     //Update is called once per frame
     void Update()
     {
+        //float cameraPos = 0;
+        //cameraPos = Remap(proxData,217,0,zoomMin, zoomMax); // map the data
+        //cameraPos = Clamp(cameraPos, zoomMin, zoomMax); // clamp the data
+        //CameraModifier.transform.position = new Vector3(CameraModifier.transform.position.x, CameraModifier.transform.position.y, cameraPos); // move camera z to new pos
+        zoomCameras();
+        //toggleAnimation();
+        switchScene();
+        updateHues();
     }
 
     //Invoked when a line of data is received from the serial device.
@@ -52,22 +69,112 @@ public class MyListener : MonoBehaviour
         proxData = arduinoData.Prox;
 
         bttnData = arduinoData.Bttn;
-     
+
 
         // Zoom Camera
-        float cameraPos = 0;
-        if (dialPercent != dialPercentPrevious && (Mathf.Abs(dialPercentPrevious - dialPercent) > 2))
+        //float cameraPos = 0;
+        //if (dialPercent != dialPercentPrevious && (Mathf.Abs(dialPercentPrevious - dialPercent) > 2))
+        //{
+        //    //Debug.Log("upadated Old " + oldData);
+        //    cameraForward = dialPercentPrevious > dialPercent;
+        //    dialPercentPrevious = dialPercent;
+        //    cameraPos = Mathf.Abs(50 - dialPercent);
+        //    if (!cameraForward)
+        //    {
+        //        cameraPos = (cameraPos * -1.00f);
+        //    }
+        //}
+
+        //// Toggle Animation
+        //if (switchData == 0)
+        //{
+        //    animations = GameObject.FindGameObjectsWithTag("Animation");
+        //    foreach (GameObject a in animations)
+        //    {
+        //        AnimationManager animationM = a.GetComponent<AnimationManager>();
+        //        animationM.PauseAnimation();
+        //    }
+        //}
+        //else
+        //{
+        //    animations = GameObject.FindGameObjectsWithTag("Animation");
+        //    foreach (GameObject a in animations)
+        //    {
+        //        AnimationManager animationM = a.GetComponent<AnimationManager>();
+        //        animationM.StartAnimation();
+        //    }
+        //}
+
+        //// Switch Scene
+        //if (bttnData != prevBttnData)
+        //{
+        //    prevBttnData = arduinoData.Bttn;
+
+        //    if (bttnData == 1 && !buttonPressed)
+        //    {
+        //        buttonPressed = true;
+        //        currCamera = !currCamera;
+        //    }
+        //    else if (bttnData == 0)
+        //    {
+        //        buttonPressed = false;
+        //    }
+        //}
+
+        //Debug.Log("cameraForward " + cameraForward);
+        //Debug.Log("cameraPos " + cameraPos);
+        //CameraModifier.transform.position += new Vector3(0, 0, cameraPos * 0.5f);
+        //camScene();
+
+
+
+    }
+
+    void updateHues()
+    {
+        backgrounds = GameObject.FindGameObjectsWithTag("Background");
+        midgrounds = GameObject.FindGameObjectsWithTag("Midground");
+        foregrounds = GameObject.FindGameObjectsWithTag("Foreground");
+        float dialVal = Remap(dialPercent, 0, 100, 0, 1);
+        Color newHue = Color.HSVToRGB(dialVal, 1, 1);
+
+        if (quadrantData == 1)
         {
-            //Debug.Log("upadated Old " + oldData);
-            cameraForward = dialPercentPrevious > dialPercent;
-            dialPercentPrevious = dialPercent;
-            cameraPos = Mathf.Abs(50 - dialPercent);
-            if (!cameraForward)
+            foreach (GameObject b in backgrounds)
             {
-                cameraPos = (cameraPos * -1.00f);
+                b.GetComponent<SpriteRenderer>().color = newHue;
+            }
+        }
+        if (quadrantData == 2)
+        {
+            foreach (GameObject m in midgrounds)
+            {
+                m.GetComponent<SpriteRenderer>().color = newHue;
+            }
+        }
+        if (quadrantData == 3)
+        {
+            foreach (GameObject f in foregrounds)
+            {
+                f.GetComponent<SpriteRenderer>().color = newHue;
             }
         }
 
+    }
+    void zoomCameras()
+    {
+        float cameraPos = 0;
+        cameraPos = Remap(proxData, 217, 0, zoomMin, zoomMax); // map the data
+        cameraPos = Clamp(cameraPos, zoomMin, zoomMax); // clamp the data
+
+        // move each camera to the new z position
+        oasisCamera.transform.position = new Vector3(oasisCamera.transform.position.x, CameraModifier.transform.position.y, cameraPos);
+        tropicCamera.transform.position = new Vector3(tropicCamera.transform.position.x, CameraModifier.transform.position.y, cameraPos);
+        spaceCamera.transform.position = new Vector3(spaceCamera.transform.position.x, CameraModifier.transform.position.y, cameraPos);
+    }
+
+    void toggleAnimation()
+    {
         // Toggle Animation
         if (switchData == 0)
         {
@@ -87,31 +194,8 @@ public class MyListener : MonoBehaviour
                 animationM.StartAnimation();
             }
         }
-
-        // Switch Scene
-        if (bttnData != prevBttnData)
-        {
-            prevBttnData = arduinoData.Bttn;
-
-            if (bttnData == 1 && !buttonPressed)
-            {
-                buttonPressed = true; 
-                currCamera = !currCamera; 
-            }
-            else if (bttnData == 0)
-            {
-                buttonPressed = false;
-            }
-        }
-
-        //Debug.Log("cameraForward " + cameraForward);
-        //Debug.Log("cameraPos " + cameraPos);
-        CameraModifier.transform.position += new Vector3(0, 0, cameraPos * 0.5f);
-        camScene();
-
-
-
     }
+
     //Data is a string in this format: "Dial:100,Switch:0"
     void parseArduinoData(string data)
     {
@@ -123,6 +207,38 @@ public class MyListener : MonoBehaviour
         arduinoData.Prox = float.Parse(ardData[3].Split(":")[1]);
         //Debug.Log("Dial: " + arduinoData.Dial);
         //Debug.Log("Switch: " + arduinoData.Switch);
+    }
+
+    public float Remap(float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var fromAbs = from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
+
+        var normal = fromAbs / fromMaxAbs;
+
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+
+        var to = toAbs + toMin;
+
+        return to;
+
+    }
+
+    public float Clamp(float val, float min, float max)
+    {
+        if (val < max)
+        {
+            return max;
+        }
+        else if (val > min)
+        {
+            return min;
+        }
+        else
+        {
+            return val;
+        }
     }
 
     public class ArduinoDataClass
@@ -145,21 +261,30 @@ public class MyListener : MonoBehaviour
     {
         oasisCamera.enabled = true;
         tropicCamera.enabled = false;
+        spaceCamera.enabled = false;
 
-        Debug.Log(tropicCamera.isActiveAndEnabled);
+        //Debug.Log(tropicCamera.isActiveAndEnabled);
 
     }
-    public void camScene()
+    public void switchScene()
     {
-        if (currCamera)
+        if (switchData == 0)
         {
-            tropicCamera.enabled = false;
             oasisCamera.enabled = true;
+            tropicCamera.enabled = false;
+            spaceCamera.enabled = false;
         }
-        else
+        else if (switchData == 1)
         {
-            tropicCamera.enabled = true;
             oasisCamera.enabled = false;
+            tropicCamera.enabled = true;
+            spaceCamera.enabled = false;
+        }
+        else if (switchData == 2)
+        {
+            oasisCamera.enabled = false;
+            tropicCamera.enabled = false;
+            spaceCamera.enabled = true;
         }
     }
 }
