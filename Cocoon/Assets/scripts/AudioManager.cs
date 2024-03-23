9cf68d8b2955eaf425a1a55b782cd7af158fcc64
CursedMushroom <osl2877@rit.edu>
2024-03-23 19:36:05 -0400

    added frog and vector bird

---
+++ Cocoon/Assets/scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+   public AudioClip backingTrack1;
+    public AudioClip backingTrack2;
+    public AudioClip backingTrack3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
